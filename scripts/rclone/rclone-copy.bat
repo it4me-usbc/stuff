4dc020e87390b79c641e79ab6cce287e59e449d3
it4me-usbc <131489645+it4me-usbc@users.noreply.github.com>
2023-04-24 13:41:36 -0700

    Update rclone-copy.bat

--- scripts/rclone/rclone-copy.bat
+++ scripts/rclone/rclone-copy.bat
@@ -1,15 +1,16 @@
 @echo off
-REM The purpose of this batch file is to be flexible enough for anyone to adapt to their settings
-REM Set the parameters like the Rclone location the config name and where the folders are located
+ECHO The purpose of this batch file is to be flexible enough for anyone to adapt to their settings
+ECHO Set the parameters like the Rclone location the config name and where the folders are located
 
-REM ========================
-REM When you type out the full command it would look like this.
-REM 
-REM c:\rclone\rclone.exe copy "C:\Users\Username\Documents" fileserver:/username
-REM
-REM
+ECHO ========================
+ECHO When you type out the full command it would look like this.
+ECHO . 
+ECHO c:\rclone\rclone.exe copy "C:\Users\Username\Documents" fileserver:/username
+ECHO .
+ECHO ================
 
-REM Set the configuration name of the file server you are calling
+REM Set the configuration stuff here
+
 SET RC=c:\rclone\rclone.exe
 SET RC-CONFIG=fileserver
 SET HOME=C:\Users\username
