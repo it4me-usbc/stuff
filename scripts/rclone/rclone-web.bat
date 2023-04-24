dd5972fc3af88e84238ce18279b165673f407987
it4me-usbc <131489645+it4me-usbc@users.noreply.github.com>
2023-04-24 14:17:17 -0700

    Update rclone-web.bat

--- scripts/rclone/rclone-web.bat
+++ scripts/rclone/rclone-web.bat
@@ -1,3 +1,3 @@
 @ECHO Off
-REM This launches rclone web GUI on localhost to make it easier to configure.
+REM This launches rclone web GUI on localhost to make it easier to configure. Place it in the same folder as rclone.exe in Windows.
 .\rclone rcd --rc-web-gui
